91deadb13bea78bf09b6f402550d39bb1225d0e9
Oliwer Helsén <oliwer.helsen@live.com>
2015-01-04 01:22:56 +0100

    Fixed a minor issue

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var _ = require('lodash');
  * Extend Billogram module
  */
 module.exports = _.extend(
-	require('./lib/billogram.invoices'),
-	require('./lib/billogram.customers'),
+	/*require('./lib/billogram.invoices'),
 	require('./lib/billogram.items'),
 	require('./lib/billogram.settings'),
-	require('./lib/billogram.logotype')
+	require('./lib/billogram.logotype'),*/
+	require('./lib/billogram.customers')
 );
